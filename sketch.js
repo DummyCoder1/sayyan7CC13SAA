690072aae70b7148e983e7481973b640fc6563d2
DummyCoder1 <100186733+DummyCoder1@users.noreply.github.com>
2022-04-07 11:34:08 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,31 @@
+var colors = ["white","black","red","green","blue"];
+function setup() {
+  createCanvas(innerWidth, innerHeight);
+
+  background("white");
+  
+  header();
+  console.log(colors);
+ for(var i = 0; i<colors.length; i++){
+   console.log(colors[i]);
+  
+ }
+}
+
+
+function draw() {
+ 
+}
+
+//display header
+function header() {
+  fill("#404040");
+  rect(0, 0, width, 50);
+
+  textAlign(CENTER);
+  fill("white");
+  textSize(18);
+  text("PAINT", width / 2, 33);
+}
+    
+
